ddfa77e297128a8b684390145946851e951b2d25
Leonardo Alegre <leonardo.alegre.c@gmail.com>
2020-11-27 19:47:52 +0000

    Refactorización cofigo datatables

--- public/assets/js/datatables.search.js
+++ public/assets/js/datatables.search.js
@@ -1,16 +1,40 @@
-function initDataTable(options = {}){
-    
+/* global $*/
+function initDataTable(id, options = {}){
+    let dataTableOpt = options.dataTable;
     let defaultOpts = {
         buttonsResponsive: [],
         filtro: false
-    }
+    };
     
+    let defaultDataTable = {
+        dom: 'lprt',
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json'
+        },
+        ordering: false,
+        serverSide: false,
+        ajax: undefined,
+        
+        columnDefs: undefined
+    };
+    
+    dataTableOpt = Object.assign(defaultDataTable, dataTableOpt);
     options = Object.assign(defaultOpts, options);
     
-    let table = $('#table_busqueda_socio').DataTable({
-        responsive: true,
-        dom: 'ft',
-        ordering: false,
+    // Iniciar Tabla
+    let table = $(id).DataTable({
+        
+        dom: dataTableOpt.dom,
+        language: dataTableOpt.language,
+        ordering: dataTableOpt.ordering,
+        
+        // Server
+        serverSide: dataTableOpt.serverSide,
+        ajax: dataTableOpt.ajax,
+        
+        // Width
+        columnDefs: dataTableOpt.columnDefs,
+        
         responsive: {
             details: {
                 renderer: ( api, rowIdx, columns ) => {
@@ -42,103 +66,166 @@ function initDataTable(options = {}){
                         }
                         return '';
                     }).join('');
-    
                     return `<ul data-dtr-index="${rowIdx}" class="dtr-details">
                         ${data}
                     </ul>`;
                 }
-                
             }
         }
     });
     
-    if(!!options.filtro){
-        table.filter = {
-            hidden: true,
-            row: undefined
-        }
-        let btnFilter = document.createElement('button');
-        btnFilter.classList.add('btn', 'btn-danger', 'btnFilter');
-        btnFilter.innerHTML = `<i class="material-icons">filter_list</i>`;
-        btnFilter.addEventListener('click', e => {
-            if(table.filter.hidden){
-                table.filter.hidden = false;
-                table.body()[0].prepend(table.filter.row);
-            }else{
-                table.filter.hidden = true;
-                table.filter.row.remove();
+    table.on('init', () => {
+       
+        // Iniciar row de filtros
+        if(!!options.filtro){
+            table.filter = {
+                hidden: true,
+                row: undefined,
+                index: undefined
+            };
+            let btnFilter = document.createElement('button');
+            btnFilter.classList.add('btn', 'btn-danger', 'btnFilter');
+            btnFilter.innerHTML = `<i class="material-icons">filter_list</i>`;
+            btnFilter.addEventListener('click', e => {
+                if(table.filter.hidden){
+                    table.filter.hidden = false;
+                    table.body()[0].prepend(table.filter.row);
+                }else{
+                    table.filter.hidden = true;
+                    table.filter.row.remove();
+                }
+            });
+            table.table().header().querySelector('th').appendChild(btnFilter);
+            
+            let columns = table.columns().count();
+            let row = document.createElement('tr');
+            row.classList.add('rowFilter');
+            let filtros = [];
+            for(let obj of options.filtro){
+                filtros[obj.index] = obj;
             }
-        });
-        table.table().header().querySelector('th').appendChild(btnFilter);
-        
-        let columns = table.columns().count();
-        let row = document.createElement('tr');
-        row.classList.add('rowFilter');
-        let filtros = [];
-        for(let obj of options.filtro){
-            filtros[obj.index] = obj;
+            
+            let hiddenCols = table.columns().responsiveHidden();
+            
+            
+            
+            for(let colIndex = 0; colIndex < columns; colIndex++){
+                
+                let td = document.createElement('td');
+                
+                if(colIndex == 0){
+                    td.innerHTML = `<button class="btn btn-danger clearFilter"><i class="material-icons">delete</i></button>`;
+                    td.querySelector('button').addEventListener('click', clearFilter);
+                    row.appendChild(td);
+                    continue;
+                }
+                
+                if(!hiddenCols[colIndex]){
+                    td.style.display = 'none';
+                }
+                
+                if(!!filtros[colIndex]){
+                    switch (filtros[colIndex].type) {
+                        case 'text':
+                            td.innerHTML = `<input type="text" col-index="${colIndex}">`;
+                            td.querySelector('input').addEventListener('input', search);
+                            break;
+                        case 'select':
+                            let select = document.createElement('select');
+                            select.setAttribute('col-index', colIndex);
+                            select.addEventListener('change', search);
+                            td.appendChild(select);
+                            
+                            let opt = document.createElement('option');
+                            opt.value = 'T';
+                            opt.text = 'Todo';
+                            select.add(opt);
+                            for(let option of filtros[colIndex].options){
+                                let opt = document.createElement('option');
+                                if(typeof option == 'string'){
+                                    opt.value = option;
+                                    opt.text = option;
+                                }else{
+                                    opt.value = option.value;
+                                    opt.text = option.text;
+                                }
+                                
+                                select.add(opt);
+                            }
+                            break;
+                    }
+                }
+                row.appendChild(td);
+            }
+            
+            table.filter.row = row;
+            
+            table.on('responsive-resize', (e, dataTable, columns) => {
+               if(!table.filter.hidden){
+                   let tds = table.body()[0].querySelectorAll('.rowFilter td');
+                   for(let i = 0; i < tds.length; i++){
+                       tds[i].style.display = columns[i] ? '' : 'none';
+                   }
+               }
+            });
+            
+            table.on('draw', (e) => {
+                if(!table.filter.hidden){
+                    table.body()[0].prepend(table.filter.row);
+                    
+                    if(!!table.filter.index){
+                        // Si hay un inpu seleccionado, darle focus
+                        let input = table.body()[0].querySelector(`.rowFilter [col-index="${table.filter.index}"]`);
+                        if(input.tagName == 'INPUT'){
+                            input.focus();
+                            input.selectionStart = input.value.length;
+                            input.selectionEnd = input.value.length;
+                        }
+                    }
+                }
+            });
+            
         }
         
-        for(let i = 0; i < columns; i++){
-            let td = document.createElement('td');
-            if(!!filtros[i]){
-                switch (filtros[i].type) {
-                    case 'text':
-                        td.innerHTML = '<input type="text">';
+        function search(e){
+            let colIndex = e.currentTarget.getAttribute('col-index');
+            let value = e.currentTarget.value;
+            
+            switch (e.currentTarget.tagName) {
+                case 'INPUT':
+                    table.filter.index = colIndex;
+                    table.column(colIndex).search(value);
+                    break;
+                
+                case 'SELECT':
+                    if(value == 'T'){
+                        table.column(colIndex).search('');
+                    }else{
+                        table.column(colIndex).search(value);
+                    }
+                    break;
+            }
+            table.draw();
+        }
+        
+        function clearFilter(e){
+            let filters = e.currentTarget.closest('.rowFilter').querySelectorAll('[col-index]');
+            for(let element of filters){
+                switch (element.tagName) {
+                    case 'INPUT':
+                        element.value = '';
                         break;
-                    case 'select':
-                        let select = document.createElement('select');
-                        td.appendChild(select);
-                        for(let option of filtros[i].options){
-                            let opt = document.createElement('option');
-                            opt.value = option;
-                            opt.text = option;
-                            select.add(opt);
-                        }
+                    case 'SELECT':
+                        element.value = 'T';
                         break;
                 }
+                table.columns(element.getAttribute('col-index')).search('');
             }
-            row.appendChild(td);
+            table.draw();
         }
-        
-        table.filter.row = row;
-        
-        table.on('responsive-resize', (e, dataTable, columns) => {
-           if(!table.filter.hidden){
-               let tds = table.body()[0].querySelectorAll('.rowFilter td');
-               for(let i = 0; i < tds.length; i++){
-                   tds[i].style.display = columns[i] ? '' : 'none';
-               }
-           }
-        });
-        // table.on('draw', (e) => {
-        //     let row = e.currentTarget.insertRow(1);
-        //     row.innerHTML = `
-        //     <td></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>
-        //     <td><input type="text"></td>`;
-        //     row.classList.add('filtros');
-        //     console.log(row);
-        // });
-    }
-    // .on('init', () => {
-    //     setInterval(() => {
-    //         $.fn.dataTable.tables( { visible: true, api: true } ).columns.adjust();
-    //     }, 500);
-    // }).on('responsive-display', (e, t, row, showHide, update) => {
-    //     if(showHide){
-    //         console.log(row);
-    //     }
-    // });
+    
+    });
+    
     return table;
 }
 
--- resources/views/datos-maestros/socio/search.blade.php
+++ resources/views/datos-maestros/socio/search.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Datos Maestros - Socio de Negocio')
 
 @section('css')
-<link rel="stylesheet" href="{{ URL::asset('assets/css/tableSearch.css') }}">
+<!--<link rel="stylesheet" href="{{ URL::asset('assets/css/tableSearch.css') }}">-->
 @endsection
 
 @section('breadcrumb')
@@ -79,7 +79,7 @@
                         <a class="btn btn-primary show" href="/datos-maestros/socio/3"><i class="material-icons">visibility</i></a>
                     </td>
                     <td>C</td>
-                    <td>Valenza Araos, Gloria Lastenia</td>
+                    <td>Valenza Araos, Gloria Qwery</td>
                     <td>31005453</td>
                     <td>N</td>
                     <td>TPN</td>
@@ -103,23 +103,31 @@
 @section('js')
 <script src="{{ URL::asset('assets/js/datatables.search.js') }}"></script>
 <script>
-    const table_search = initDataTable({
-        // {index: 2, type: 'href', value: '.btn.show'}
-        // {index: 11, type: 'function', value: 'hola'}
+    const table_search = initDataTable('#table_busqueda_socio', {
+        dataTable: {
+            dom: `<"row"
+                <"col-12 col-md-4"l>
+                <"col-12 col-md-8"p>
+            >rt`,
+            columnDefs: [
+                {className: 'text-center', width: "40px", targets: [0,2,4,5,6,7,8,9,10,11,12]},
+                {width: "130px", targets: 1}
+            ]
+        },
         buttonsResponsive : [{index: 1, type: 'href', value: '.btn.show'}],
         filtro: [
             {index: 1, type: 'text'},
-            {index: 2, type: 'select', options: ['C']},
+            {index: 2, type: 'select', options: [{text: 'C', value: 'C'}]},
             {index: 3, type: 'text'},
             {index: 4, type: 'text'},
-            {index: 5, type: 'select', options: ['Si', 'No']},
+            {index: 5, type: 'select', options: [{text: 'Si', value: 'S'}, {text: 'No', value: 'N'}]},
             {index: 6, type: 'select', options: ['TPN']},
             {index: 7, type: 'text'},
             {index: 8, type: 'text'},
             {index: 9, type: 'select', options: ['Si', 'No']},
             {index: 10, type: 'select', options: ['Igv']},
             {index: 11, type: 'select', options: ['Contado', 'Credito']},
-            {index: 12, type: 'select', options: ['Si', 'No']}
+            {index: 12, type: 'select', options: [{text: 'Si', value: 'S'}, {text: 'No', value: 'N'}]}
         ]
     });
 </script>
